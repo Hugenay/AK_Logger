844e53662b37bcc66ebde40e5d25e204c9322080
JonathanG <jograesser@gmail.com>
2014-04-16 21:14:34 +0200

    update Mux und SD-Karte

--- Drucksensortest_SD/Drucksensortest_SD.ino
+++ Drucksensortest_SD/Drucksensortest_SD.ino
@@ -1,10 +1,8 @@
-#include <VM_Boards.h>
+
 #include <i2c_t3.h>
 #include <SD.h>
 
-Sd2Card card;
-SdVolume volume;
-SdFile root;
+
 
 #define PRESSURE_SENSOR_ADRESS 0x78   // = 0b1111000
 #define OUTMIN 0x0666
@@ -13,79 +11,86 @@ SdFile root;
 #define SENSIVITY 1045.56    //25mb unidirekt
 #define AIRDENSITY 1.2041
 
+//Mux control pins
+int s0 = 2;
+int s1 = 3;
+int s2 = 4;
+int s3 = 5;
+
 uint16_t rawPressure;
 double newSpeed, Pressure, pressureh2o;
 const int chipSelect = 10; //Adafruit SD-Board
 
 void setup()
 {
-  Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
+   // Open serial communications and wait for port to open:
   Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+    //Mux pinout setup
+  pinMode(s0, OUTPUT); 
+  pinMode(s1, OUTPUT); 
+  pinMode(s2, OUTPUT); 
+  pinMode(s3, OUTPUT); 
+
+  digitalWrite(s0, LOW);
+  digitalWrite(s1, LOW);
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, LOW);
+
 
   Serial.print("Initializing SD card...");
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
   pinMode(10, OUTPUT);
-
+  
   // see if the card is present and can be initialized:
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+  if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
     return;
+  
   }
   Serial.println("card initialized.");
-
+  //Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
+  //Serial.println("wire initialized.");
+ 
 }
+
 
 void loop()
 {
-
-  uint8_t msb, lsb;
+ 
+ uint8_t msb, lsb;
   String dataString = "";
 
+  SelectChannel(6);
+  Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
+  Serial.println("wire initialized.");
   Wire.requestFrom(0x78, 2, I2C_STOP);
+  
   while(Wire.available())   
   { 
     msb = Wire.read(); 
     lsb = Wire.read();
   }
-
-
-  //Serial.print(msb,HEX);
-  //Serial.print("\t");
-  //Serial.print(lsb,HEX);
-  //Serial.print("\t");
-
-
-
+  
+  Serial.print("MSB: ");
+  Serial.println(msb);
+  Serial.print("LSB: ");
+  Serial.println(lsb);
 
   rawPressure = (int)(msb << 8) | lsb;
-
-  //Serial.print(rawPressure,HEX);
-  //Serial.print("\t");
-
-
-  Pressure = (rawPressure - OUTMIN) / SENSIVITY;
-
+  Pressure = (rawPressure - OUTMIN) / SENSIVITY; 
   pressureh2o = Pressure * 10.197;
-
   newSpeed = sqrt( 2 * Pressure / AIRDENSITY);
-
-  //Serial.print(Pressure);
-  //Serial.print(" mbar");
-  //Serial.print("\t");
-  //Serial.print(pressureh2o);
-  //Serial.print(" mmH2O");
-  //Serial.print("\t");
-  //Serial.print(newSpeed);
-  //Serial.println(" m/s");
-
-
-
+  
   dataString = String(msb) + "\t" + String(lsb) + "\t" + String(rawPressure) +"\t" + String(Pressure) +"mbar\t" + String(pressureh2o) + "mmH20\t" + String(newSpeed)+ "m/s";
 Serial.println(dataString);
-  File dataFile = SD.open("Messdaten.txt", FILE_WRITE);
-
+ 
+  File dataFile = SD.open("data.txt", FILE_WRITE);
+  
   if (dataFile) {
     dataFile.println(dataString);
     dataFile.close();
@@ -94,10 +99,42 @@ Serial.println(dataString);
   else {
     Serial.println("Sava fail");
   }
+  
   delay(1000);
-
+ 
 }
 
+int SelectChannel (int channel){
+  int controlPin[] = {s0, s1, s2, s3};
 
+  int muxChannel[16][4]={
+    {0,0,0,0}, //channel 0
+    {1,0,0,0}, //channel 1
+    {0,1,0,0}, //channel 2
+    {1,1,0,0}, //channel 3
+    {0,0,1,0}, //channel 4
+    {1,0,1,0}, //channel 5
+    {0,1,1,0}, //channel 6
+    {1,1,1,0}, //channel 7
+    {0,0,0,1}, //channel 8
+    {1,0,0,1}, //channel 9
+    {0,1,0,1}, //channel 10
+    {1,1,0,1}, //channel 11
+    {0,0,1,1}, //channel 12
+    {1,0,1,1}, //channel 13
+    {0,1,1,1}, //channel 14
+    {1,1,1,1}  //channel 15
+  };
+
+  //loop through the 4 sig
+  for(int i = 0; i < 4; i ++){
+    digitalWrite(controlPin[i], muxChannel[channel][i]);
+
+  }
+  Serial.print("Selected Channel ");
+  Serial.print(channel);
+  Serial.println();
+
+}
 
 
